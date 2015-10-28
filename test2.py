f56ad456958be45508396c809049922deda6b6d1
limingzhe26 <limingzhe26@hotmail.com>
2015-10-28 19:22:47 +0800

    limzlimz

---
+++ test2.py
@@ -0,0 +1,12 @@
+arr = []
+
+while True:
+	action = raw_input('input you action')
+	if action == 'add':
+		detail = raw_input('input things you')
+		arr.append(detail)
+		print arr
+	elif action == 'do':
+		if len(arr) == 0:
+			break
+		print arr.pop(0)
