7b06533ba7e4fc5b662e76f8399c0a140b0d9f0f
limingzhe26 <limingzhe26@hotmail.com>
2018-08-13 15:05:06 +0800

    Add files via upload

---
+++ hellowworld.py
@@ -0,0 +1,29 @@
+# This programe says hello
+
+'''
+Tome
+Jerry
+'''
+
+print('Hello World!')
+
+# Ask the user to input their name and assign it to the name variable
+print('What is your name?')
+myName = input()
+
+# Print out greet followed by name
+print('It is good to meet you, ' + myName)
+
+# print out the length of the name
+print ('The length of your name is ' + str(len(myName)))
+
+# Ask for your age and show how old you will be next year
+print('What is your age?')
+myAge = input()
+print ('Your will be ' +  str(int(myAge)+1) + "in a year")
+
+
+
+
+
+
